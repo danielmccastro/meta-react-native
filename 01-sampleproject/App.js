d8556d89c033549f65ba4401b227ace527f42713
Daniel Castro <danielmccastro@gmail.com>
2023-10-04 18:00:30 -0300

    created WelcomeScreen component

--- 01-sampleproject/App.js
+++ 01-sampleproject/App.js
@@ -3,6 +3,7 @@ import { View } from "react-native";
 
 import LittleLemonHeader from "./components/LittleLemonHeader";
 import LittleLemonFooter from "./components/LittleLemonFooter";
+import WelcomeScreen from "./components/WelcomeScreen"
 export default function App() {
   return (
     <>
@@ -13,6 +14,7 @@ export default function App() {
         }}
       >
         <LittleLemonHeader />
+        <WelcomeScreen />
       </View>
       <View style={{ backgroundColor: "#495E57" }}>
         <LittleLemonFooter />
--- 01-sampleproject/components/LittleLemonHeader.js
+++ 01-sampleproject/components/LittleLemonHeader.js
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 
 export default function LittleLemonHeader() {
   return (
-    <View style={{ flex: 0.1, backgroundColor: "#F4CE14" }}>
+    <View style={{ flex: 0.15, backgroundColor: "#F4CE14" }}>
       <Text style={{ marginTop: 30,fontSize: 30, color: "black", textAlign: 'center' }}>
         Little Lemon
       </Text>
