7113e567b6060b97e5c2102b4b94194246a91cf2
Daniel Castro <danielmccastro@gmail.com>
2023-10-10 17:26:01 -0300

    created a LoginPage component

--- 01-sampleproject/App.js
+++ 01-sampleproject/App.js
@@ -1,18 +1,18 @@
-import * as React from "react";
-import { View, StyleSheet } from "react-native";
+import * as React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-import LittleLemonHeader from "./components/LittleLemonHeader";
-import LittleLemonFooter from "./components/LittleLemonFooter";
-import WelcomeScreen from "./components/WelcomeScreen";
-import MenuItems from "./components/MenuItems";
+import LittleLemonHeader from './components/LittleLemonHeader';
+import LittleLemonFooter from './components/LittleLemonFooter';
+import LoginScreen from './components/LoginScreen';
+
 export default function App() {
   return (
     <>
       <View style={styles.container}>
         <LittleLemonHeader />
-        <WelcomeScreen />
+        <LoginScreen />
       </View>
-      <View style={styles.footer}>
+      <View style={styles.footerContainer}>
         <LittleLemonFooter />
       </View>
     </>
@@ -22,7 +22,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#333333",
+    backgroundColor: '#333333',
   },
-  footer: { backgroundColor: "#333333" },
+  footerContainer: { backgroundColor: '#333333' },
 });
---
+++ 01-sampleproject/components/LoginScreen.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { ScrollView, Text, StyleSheet, TextInput } from "react-native";
+
+export default function LoginScreen() {
+  const [email, onChangeEmail] = useState("");
+  const [password, onChangePassword] = useState("");
+  return (
+    <ScrollView style={styles.container}>
+      <Text style={styles.headerText}>Welcome to Little Lemon</Text>
+      <Text style={styles.regularText}>Login to continue </Text>
+      <TextInput
+        style={styles.input}
+        placeholder="email"
+        value={email}
+        onChange={onChangeEmail}
+        keyboardType="email-address"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="password"
+        value={password}
+        onChange={onChangePassword}
+        keyboardType={"default"}
+        secureTextEntry={true}
+      />
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  headerText: {
+    padding: 40,
+    fontSize: 30,
+    color: "#EDEFEE",
+    textAlign: "center",
+  },
+  regularText: {
+    fontSize: 24,
+    padding: 20,
+    marginVertical: 8,
+    color: "#EDEFEE",
+    textAlign: "center",
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    fontSize: 16,
+    borderColor: "#EDEFEE",
+    backgroundColor: "#EDEFEE",
+  },
+});
