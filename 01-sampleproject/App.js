c3b0d46bd3c068290e017dfb24a620f102ec35c7
Daniel Castro <danielmccastro@gmail.com>
2023-10-11 13:28:17 -0300

    created a drawer navigation

--- 01-sampleproject/App.js
+++ 01-sampleproject/App.js
@@ -1,15 +1,14 @@
 import * as React from "react";
 import { View, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from "@expo/vector-icons/Ionicons";
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import LittleLemonHeader from "./components/LittleLemonHeader";
 import LittleLemonFooter from "./components/LittleLemonFooter";
 import LoginScreen from "./components/LoginScreen";
 import WelcomeScreen from "./components/WelcomeScreen";
 
-const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
@@ -17,23 +16,10 @@ export default function App() {
       <NavigationContainer>
         <View style={styles.container}>
           <LittleLemonHeader />
-          <Tab.Navigator
-            screenOptions={({ route }) => ({
-              tabBarIcon: ({ size }) => {
-                let iconName;
-                if (route.name === "Welcome") {
-                  iconName = "ios-home";
-                } else if (route.name === "Login") {
-                  iconName = "ios-enter";
-                }
-                return <Ionicons name={iconName} size={size} />;
-              },
-            })}
-            initialRouteName="Login"
-          >
-            <Tab.Screen name="Welcome" component={WelcomeScreen} />
-            <Tab.Screen name="Login" component={LoginScreen} />
-          </Tab.Navigator>
+          <Drawer.Navigator useLegacyImplementation initialRouteName="Login">
+            <Drawer.Screen name="Welcome" component={WelcomeScreen} />
+            <Drawer.Screen name="Login" component={LoginScreen} />
+          </Drawer.Navigator>
         </View>
         <View style={styles.footerContainer}>
           <LittleLemonFooter />
@@ -46,7 +32,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#333333",
+    backgroundColor: '#333333',
   },
-  footerContainer: { backgroundColor: "#333333" },
+  footerContainer: { backgroundColor: '#333333' },
 });
